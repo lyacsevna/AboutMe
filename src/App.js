342d1f0ba25ca407d6cb0ec259ff5dab3effb05d
Sofia <sonechka68zvr@gmail.com>
2025-01-23 12:22:14 +0300

    Resolved merge conflicts

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function MainInfo() {
+  return (
+    <div className='MainInfo'>
+      <p>привет</p>
+    </div>
+
+
+  );
+}
+
+function App() {
+  return (
+    <div className="App">
+        <p>привет</p>
+
+    </div>
+  );
+}
+
+export default App;
