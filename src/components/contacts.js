1d089089cb422bc568e60d5060780f7527423f34
Sofia <sonechka68zvr@gmail.com>
2025-02-01 16:18:25 +0300

    edit contact style

--- src/components/contacts.js
+++ src/components/contacts.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import '../styles/contacts.css'
 
 const Contacts = () => {
     return (
         <section id="contacts">
             <h2>Контакты</h2>
-            <a href = "https://github.com/lyacsevna">Мой профиль на GitHub</a> 
+            <a className = "contact-item" href = "https://github.com/lyacsevna">Мой профиль на GitHub</a> 
         </section>
     );
 };
--- src/components/projects.js
+++ src/components/projects.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../styles/projects.css';
 import { myprojects } from "../data";
 
 function ProjectsList({ projectTitle, description, stack }) {
