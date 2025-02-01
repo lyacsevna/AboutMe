5d515fbfe82be2ac772c156287c714d8a98cc216
Sofia <sonechka68zvr@gmail.com>
2025-02-01 15:20:35 +0300

    create animation for hello world

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React , { useEffect, useState }  from 'react';
 
 const AboutMe = () => {
+    const [text, setText] = useState('');
+    const fullText = "Hello, world!";
+
+    useEffect(() => {
+        let index = 0;
+        const interval = setInterval(() => {
+            setText(fullText.substring(0, index + 1));
+            index++;
+            if (index === fullText.length) {
+                clearInterval(interval);
+            }
+        }, 100);
+
+        return () => clearInterval(interval);
+    }, [fullText]);
+
     return (
         <section id="aboutme">
-            <h1>Hello, world!</h1>
-
-            <p> Привет! Это мой сайт, созданный для изучения React.js.
-                В процессе этот сайт превратится в небольшое портфолио.
+            <h1>{text}</h1>
+            <p>
+                Привет! Это мой сайт, созданный для изучения React.js. 
+                В процессе этот сайт превратится в небольшое портфолио. 
                 Пока что на этом все.
             </p>
             <p>I am just learning</p>
