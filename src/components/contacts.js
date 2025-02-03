51df56b27dac994f1fad15fd2dbf75c164d72f44
Sofia <sonechka68zvr@gmail.com>
2025-02-03 13:18:20 +0300

    added contact form

--- src/components/contacts.js
+++ src/components/contacts.js
@@ -3,10 +3,21 @@ import '../styles/contacts.css'
 
 const Contacts = () => {
     return (
-        <section id="contacts">
+        <><section id="contacts">
             <h2>Контакты</h2>
-            <a className = "contact-item" href = "https://github.com/lyacsevna">Мой профиль на GitHub</a> 
-        </section>
+            <a className="contact-item" href="https://github.com/lyacsevna">Мой профиль на GitHub</a>
+        </section><h3>Форма обратной связи</h3><form className="contact-form">
+                <label htmlFor="name">Имя:</label>
+                <input type="text" id="name" name="name" required />
+
+                <label htmlFor="email">Email:</label>
+                <input type="email" id="email" name="email" required />
+
+                <label htmlFor="message">Сообщение:</label>
+                <textarea id="message" name="message" rows="4" required></textarea>
+
+                <button type="submit">Отправить</button>
+            </form></>
     );
 };
 
