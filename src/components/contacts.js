8b515480cdfca65c676a9cc0f87f5caafe4af49d
Sofia <sonechka68zvr@gmail.com>
2025-02-05 20:00:19 +0300

    edit contact css

--- src/components/contacts.js
+++ src/components/contacts.js
@@ -5,29 +5,35 @@ import '../styles/contacts.css'
 
 const Contacts = () => {
     return (
-        <><section id="contacts">
-            <h2>Контакты</h2>
+        <section id="contacts">
+            <header>
+                <h2>Контакты</h2>
+            </header>
             <div className='contactLink'>
-            <a href="https://github.com/lyacsevna" target="_blank" rel="noopener noreferrer">
+                <a href="https://github.com/lyacsevna" target="_blank" rel="noopener noreferrer" aria-label="GitHub">
                     <img src={githubIcon} alt="GitHub" className="icon" />
-            </a>
-            <a
-                href="https://vk.com/" target="_blank" rel="noopener noreferrer">
+                </a>
+                <a href="https://vk.com/" target="_blank" rel="noopener noreferrer" aria-label="VK">
                     <img src={vkIcon} alt="VK" className="icon" />
                 </a>
             </div>
-        </section><h3>Форма обратной связи</h3><form className="contact-form">
-                <label htmlFor="name">Имя:</label>
-                <input type="text" id="name" name="name" required />
+            <h3>Форма обратной связи</h3>
+            <form className="contact-form" aria-labelledby="contacts">
+                <fieldset>
+                    <legend>Свяжитесь с нами</legend>
+                    <label htmlFor="name">Имя:</label>
+                    <input type="text" id="name" name="name" required />
 
-                <label htmlFor="email">Email:</label>
-                <input type="email" id="email" name="email" required />
+                    <label htmlFor="email">Email:</label>
+                    <input type="email" id="email" name="email" required />
 
-                <label htmlFor="message">Сообщение:</label>
-                <textarea id="message" name="message" rows="4" required></textarea>
+                    <label htmlFor="message">Сообщение:</label>
+                    <textarea id="message" name="message" rows="4" required></textarea>
 
-                <button type="submit">Отправить</button>
-            </form></>
+                    <button type="submit">Отправить</button>
+                </fieldset>
+            </form>
+        </section>
     );
 };
 
