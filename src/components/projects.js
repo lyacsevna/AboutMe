5e457448bc2a4285889616ac469756d6722e84f1
Sofia <sonechka68zvr@gmail.com>
2025-02-06 21:37:03 +0300

    editing styles

--- src/components/projects.js
+++ src/components/projects.js
@@ -5,14 +5,16 @@ import { myprojects } from "../data";
 function ProjectsList({ projectTitle, description, stack }) {
     function handleClick() {
       localStorage.setItem('auth-time', new Date())
-    }
+  }
   
-    return (
+  return (
+    <div className='project_item'>
       <p onClick={handleClick}>
           <strong>{projectTitle}</strong>
           <p>{description}</p>
           <p>{stack}</p>
       </p>
+      </div>
     )
   }
 
@@ -25,9 +27,8 @@ const Projects = () => {
             
         <div>
             {myprojects.map((proj) => (
-              <div className='project_item'>
+          
                 <ProjectsList key = { proj.projectTitle } {...proj}/>
-              </div>
             ))}
         </div>
       </section>
