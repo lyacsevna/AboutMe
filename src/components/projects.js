c8c4367de204b72a4bf53d249c651bbca0b83b03
Sofia <sonechka68zvr@gmail.com>
2025-02-06 21:52:53 +0300

    removing duplicate styles

--- src/components/projects.js
+++ src/components/projects.js
@@ -8,26 +8,24 @@ function ProjectsList({ projectTitle, description, stack }) {
   }
   
   return (
-    <div className='project_item'>
-      <p onClick={handleClick}>
-          <strong>{projectTitle}</strong>
-          <p>{description}</p>
-          <p>{stack}</p>
+    <div className='project_item' onClick={handleClick}>
+      <p>
+        <strong>{projectTitle}</strong>
+        <span>{description}</span>
+        <span>{stack}</span>
       </p>
-      </div>
-    )
-  }
+    </div>
+  );
+}
 
 
 const Projects = () => {
     return (
       <section id="projects_container">
             <h2>Мои проекты</h2>
-            <p>Ниже вы можете ознакомиться с моими проектами и их кратким описанием.</p>
-            
+            <p>Ниже вы можете ознакомиться с моими проектами и их кратким описанием.</p>            
         <div>
             {myprojects.map((proj) => (
-          
                 <ProjectsList key = { proj.projectTitle } {...proj}/>
             ))}
         </div>
